d3674812ec0937bd36a2b02d1639ec0b201d04b1
Anand_G99 <anand@detecttechnologies.com>
2023-04-10 15:14:08 +0530

    created the recipe api's

--- recipe/serializers.py
+++ recipe/serializers.py
@@ -9,7 +9,18 @@ from django.utils.translation import gettext as _
 
 
 class RecipeSerializer(serializers.ModelSerializer):
+    """Serializer for recipe"""
     class Meta:
         model = Recipe
         fields = ('id', 'title', 'time_minutes', 'price', 'link')
         read_only_fields = ['id']
+
+
+# NOTE9 : to create a recipe detail api
+# create the serializer - extension of RecipeSerializer
+# add an extra field description
+# modify the recipeViewSet (check there for more info)
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serializer for recipe detail view"""
+    class Meta(RecipeSerializer.Meta):
+        fields = RecipeSerializer.Meta.fields + ('description',)
--- recipe/views.py
+++ recipe/views.py
@@ -1,3 +1,5 @@
+"""Recipe view"""
+
 # from django.shortcuts import render
 # from rest_framework import generics, authentication, permissions
 # from rest_framework.authtoken.views import ObtainAuthToken
@@ -7,22 +9,42 @@ from core.models import Recipe
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
-from recipe.serializers import RecipeSerializer
+from recipe import serializers
 
 # Create your views here.
 
 # to use
+# admintest@example.com
+# pass123*
 
 
-class RecipeViewSet(viewsets.ViewSet):
+class RecipeViewSet(viewsets.ModelViewSet):
     """Manage recipe """
-    serializer_class = RecipeSerializer
+    # serializer_class = serializers.RecipeSerializer
+    serializer_class = serializers.RecipeDetailSerializer
     queryset = Recipe.objects.all()
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
     # override
-    # typicallu we return all of the objects
+    # typically we return all of the objects
     # here we are filtering for the specific user(we already know they are authenticated cuz of the authenticated_classes)
     def get_queryset(self):
         return self.queryset.filter(user=self.request.user).order_by('-id')
+
+    # overide so we canfigure the serializer class based on action
+    # set the default serializer as the RecipeDeatilSerializer (we use it a lot more)
+    # if teh action is listing then we use recipeserialzier else use teh deafult serialiser class we set
+    def get_serializer_class(self):
+        """Return the serializer class for request"""
+        if self.action == 'list':
+            return serializers.RecipeSerializer
+
+        return self.serializer_class
+
+    # NOTE10: override
+    # when we perform the creation of a new recipe through this viewset,we call this method,
+    # when we save we will save the user value to the current authenticated user(performed by serializer)
+    def perform_create(self, serializer):
+        """Create a new recipe"""
+        serializer.save(user=self.request.user)
